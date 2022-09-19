295cfbe462854ab8a76dd452851024cb2ff26bee
Ryuiru <gediminas.gedvilas9@gmail.com>
2022-09-19 13:39:21 +0300

    asd
    asd

--- JavaScript/js/1 uzduotis.js
+++ JavaScript/js/1 uzduotis.js
@@ -4,21 +4,39 @@
 // divas.style.height = "300px";
 // divas.style.margin = "0 auto";
 // divas.style.marginTop = "30vh";
-// divas.style.textAlign = "center"; 1 uzduotis
-function addElement () {
-    const divas = document.createElement("div");
-    divas.id = 'moku';
-    const tekstas = document.createTextNode("Valio pavyko");
-    divas.appendChild(tekstas);
-    const divas1 = document.getElementById("moku");
-    document.body.insertBefore(divas, divas1);
-    divas.style.width = "300px";
-    divas.style.height = "300px";
-    divas.style.margin = "0 auto";
-}
-function elementas(){
-    const miktukas = document.createElement('button');
-    miktukas.id = 'spaudaliukas';
-}
-addElement();
-elementas();
+// divas.style.textAlign = "center"; 
+//                                                                  1 uzduotis
+    // const divas = document.createElement("div");
+    // divas.id = 'moku';
+    // const tekstas = document.createTextNode("Valio pavyko");
+    // divas.appendChild(tekstas);
+    // const divas1 = document.getElementById("moku");
+    // document.body.insertBefore(divas, divas1);
+    // divas.style.width = "300px";
+    // divas.style.height = "300px";
+    // divas.style.margin = "0 auto";
+    // divas.style.border= "1px solid black"
+
+    // const miktukas = document.createElement('button');
+    // miktukas.id = 'spaudaliukas';
+    // miktukas.innerText = 'button';
+    // divas.appendChild(miktukas);
+    // miktukas.style.margin = "0 auto";
+    // onclick="deletus(this)";
+    // divas.addEventListener("click", () => deletus('moku'))
+    // miktukas.addEventListener("click", () => deletus('spaudaliukas'))
+
+    // function deletus(divas){
+    // document.getElementById(divas).remove()
+    // } 
+    //                                                                 2 uzduotis
+    // document.getElementById('KARAMBA').onclick = function() {
+    //     var iframe = document.createElement('iframe');
+    //     iframe.src = 'https://www.youtube.com/embed/ygI-2F8ApUM';
+    //     document.body.appendChild(iframe);
+    //     iframe.style.width = "500px";
+    //     iframe.style.height = "500px";
+    //     iframe.style.margin = "0 auto";
+    //     iframe.style.display = "block";
+    //  };
+     //Trecia uzduotis

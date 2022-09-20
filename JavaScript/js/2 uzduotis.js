d2e7560eb2cd8b3407d0b036b67bf2b502e0a1dd
Ryuiru <112540875+Ryuiru@users.noreply.github.com>
2022-09-20 14:07:15 +0300

    new enw
    new

--- JavaScript/js/2 uzduotis.js
+++ JavaScript/js/2 uzduotis.js
@@ -11,7 +11,21 @@
 //     document.write(vaisius);
 //     document.write(produktas);
 //   }
-// 2 uzduotis
+///////////////////
+// // 1. Sukuriama funkcija 2 uzduotis
+// function preke(kaina,pavadinimas){
+//   var kainaVaisiaus = kaina*5;
+//   return 'penkiu vaisiu: ' + pavadinimas + " kaina " + kainaVaisiaus;
+// }
+// var ats = preke(2, 'keksai');
+// var divas = document.createElement('div');
+// divas.innerHTML = ats;
+// // 2. atvaizduojama i ekrana
+// document.body.appendChild(divas);
+// document.body.innerHTML = ats;
+// console.log(preke(2, 'vaflis'));
+/////////////////////
+// 3 uzduotis
 // function apskaiciavimas() {
 //     var vaisius = document.getElementById('fruit').value;
 //     var kaina = document.getElementById('price').value;
@@ -23,24 +37,67 @@
 //     document.write("<br>");
 //     document.write('Pilna kaina: ',produktas,' Eurai');
 //   }
-  // 3 uzduotis
-  //masyvas
-var heroes = ["Jonas", "Petras", "Antanas", "Hansel", "Gretel"];
-//html stringas
-var asdf = document.createElement('p');
-//pereina per kiekviena heroju ir sukuria sarasa
-asdf.style.color = "#FFFFFF";
-asdf.style.backgroundColor = '#00FFFF';
-heroes.forEach(function (heroes) {
-    if(heroes.length%2==1){   
-    asdf += '<p>' + heroes ;
-    // document.querySelector('#sarasas').innerHTML = asdf;
-}   else{
-    asdf += '<p>' + heroes ;
-    document.querySelector('#sarasas').innerHTML = asdf;
-}
+//1. Sukurti HTML
+//2. Paimti reiksmes ant paspaudimo mygtuko
+// 2.1 issiaiskinti kaip paimti reiksme
+//3. Suskaiciuoti kaina
+//4. Atvaizduoti ekrane
+var btn = document.getElementById('ats');
+btn.addEventListener('click', function() {
+  var pav = document.querySelector('input [placeholder=Pavadinimas]').value;
+  var kaina = document.querySelector('input [placeholder=Kaina]').value;
+  var kiekis = document.getElementById('kiekis').value;
+  var kokiaKaina = kaina * kiekis;
+  console.log(pav, kaina);
+  var divas = document.createElement('div');
+  divas.innerHTML =
+  'Kaina lygi' +
+  kokiaKaina +
+  '. Buvo pirktas toks kiekis: ' +
+  kiekis +
+  ' ir taip vadinasi ' +
+  pav;
+  //console.log(divas);
+  document.body.appendChild(divas);
 });
-//padaro sarasa
-// asdf = '<p>' + asdf + '</p>';
-//ikeliamas kazkodel reikia # prie query selector kitep neveikia
-console.log(asdf);
+  // 4 uzduotis
+  //sukurtas masyvas su savybem
+// var heroes = [
+//   {
+//     name: "Human Male",
+//     skill: "Walk",
+//   },
+//   {
+//     name: "Computer",
+//     skill: "Be computer",
+//   },
+//   {
+//     name: "Vilkas",
+//     skill: 'Zudo',
+//   },
+// ];
+
+// 2. Per forEach atvaizduoti elementa.
+// 2.1 Issiaiskinti kaip naudot foeEach
+//2.2 Kaip gauti lygini
+//2.3 Atvaizduoti i ekrana
+//2.4 Nuspalvinti lyginius
+
+// heroes.forEach((element) => console.log(element));
+// heroes.forEach(function (element, index) {
+//   var arLyginis = index + 1;
+
+//   if(arLyginis % 2 == 0){
+//   console.log(arLyginis, element, 'ar lyginis');
+//   var divas = document.createElement('div');
+//   divas.innerHTML = element.name + " " + element.skill;
+//   divas.style.color = "white";
+//   divas.style.backgroundColor = "blue";
+//   document.body.appendChild(divas);
+// }else {
+//   var divas = document.createElement('div');
+//   divas.innerHTML = element.name + ' ' + element.skill;
+//   document.body.appendChild(divas);
+// }
+//   console.log(element, index);
+// })

0a488a85e9c752ae886fb574c07dfa5dd45c7d8e
Ryuiru <gediminas.gedvilas9@gmail.com>
2022-10-26 20:23:28 +0300

    zxc

--- hamburger/src/components/Layout/Layout.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import auxiliary from '../../hoc/auxiliary';
-const layout = (props) => (
-  <auxiliary>
-    <div></div>
-    <main>{props.children}</main>
-  </auxiliary>
-);
-export default layout;
